db40a3af04bdc16fd61bee2bd37b3039d692593b
denis ramos <pacohelmer@gmail.com>
2024-06-20 10:23:10 -0400

    hola

---
+++ src/componentes/Examen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Examen.css"
+function Examen() {
+  return (
+    <div className="cuadro">
+        <div className="primero">
+            <div className="objeto"> hola</div>
+        </div>
+        <div className="segundo">        </div>
+    </div>
+  )
+}
+
+export default Examen
